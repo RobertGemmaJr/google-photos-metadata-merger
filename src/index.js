45da9fe4333aecb5a98acea5fe6352583b4df210
Robert Gemma <38439940+RobertGemmaJr@users.noreply.github.com>
2023-12-05 15:57:23 -0500

    wip: add basic index.js and commands folder

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("INDEX");
+#!/usr/bin/env node
+
+import { program } from "commander";
+import { name, description, version } from "../package.json";
+
+program
+  // Parse package.json
+  .name(name)
+  .description(description)
+  .version(version, "-v, - version")
+  // Parse arguments
+  .parse(process.argv);
