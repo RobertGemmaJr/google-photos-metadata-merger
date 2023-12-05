72a916c2e5e29295f0ca284593da44be229d9ed1
Robert Gemma <38439940+RobertGemmaJr@users.noreply.github.com>
2023-12-05 16:06:27 -0500

    ci: Add eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,15 @@
 module.exports = {
   root: true,
-  env: { node: true, },
+  env: { 
+    node: true, 
+    es2021: true
+  },
   extends: "eslint:recommended",
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: "2021",
     sourceType: "module"
   },
-  rules: {}
+  rules: {
+    // TODO: Add runs for import order
+  }
 }
