f4079aa5a3b798f4265841c08e9d5ba681ed0111
Robert Gemma <38439940+RobertGemmaJr@users.noreply.github.com>
2023-12-05 16:07:38 -0500

    ci: Add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,15 @@
 module.exports = {
   root: true,
-  env: { 
-    node: true, 
-    es2021: true
+  env: {
+    node: true,
+    es2021: true,
   },
   extends: "eslint:recommended",
   parserOptions: {
     ecmaVersion: "2021",
-    sourceType: "module"
+    sourceType: "module",
   },
   rules: {
     // TODO: Add runs for import order
-  }
-}
+  },
+};
